ae8e9d3b9d59a88d29ce7d96096b355b9d44e73c
Sanjay Jaiswal <sanjayjaiswal586@gmail.com>
2024-07-30 14:01:54 +0530

    tailwind css setup
    Signed-off-by: Sanjay Jaiswal <sanjayjaiswal586@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./webviews/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
