03223fa0e0148361a4c418c170a6837a762bcbc9
ayesparshh <sparshgupta1001@gmail.com>
2024-09-01 07:58:10 +0000

    removed go installation

--- src/OneClickInstall.ts
+++ src/OneClickInstall.ts
@@ -1,60 +1,62 @@
-import { exec } from 'child_process';
-import * as os from 'os';
-import * as vscode from 'vscode';
+import { exec } from "child_process";
+import * as os from "os";
+import * as vscode from "vscode";
 
 export default function executeKeployOneClickCommand(): void {
-    const platform = os.platform();
-    let command: string;
+	const platform = os.platform();
+	let command: string;
 
-    if (platform === 'win32') {
-        command = `powershell -Command "iwr https://raw.githubusercontent.com/keploy/keploy/main/windows-install.ps1 -useb | iex"`;
-    } else if (platform === 'darwin' || platform === 'linux') {
-        command = `curl --silent -L https://raw.githubusercontent.com/keploy/keploy/main/install.sh | bash`;
-    } else {
-        vscode.window.showErrorMessage(`Unsupported platform: ${platform}`);
-        return;
-    }
+	if (platform === "win32") {
+		// for future use
+		command = `powershell -Command "iwr https://raw.githubusercontent.com/keploy/keploy/main/windows-install.ps1 -useb | iex"`;
+	} else if (platform === "darwin" || platform === "linux") {
+		command = `curl --silent -O -L https://keploy.io/install.sh && source install.sh`;
+	} else {
+		vscode.window.showErrorMessage(`Unsupported platform: ${platform}`);
+		return;
+	}
 
-    const terminal = vscode.window.createTerminal('Keploy Installation');
-    terminal.show();
-    terminal.sendText(command);
+	const terminal = vscode.window.createTerminal("Keploy Installation");
+	terminal.show();
+	terminal.sendText(command);
 
-    terminal.sendText('keploy --version');
+	terminal.sendText("keploy --version");
 
-    // Check if Go is installed
-    checkGoInstallation(terminal);
+	// Check if Go is installed
+	// checkGoInstallation(terminal);
 }
 
-function checkGoInstallation(terminal: vscode.Terminal): void {
-    exec('go version', (error, stdout, stderr) => {
-        if (error) {
-            vscode.window.showWarningMessage('Go is not installed. Installing Go is recommended for using Keploy with Go projects.');
-            const installGoCommand = getGoInstallCommand();
-            if (installGoCommand) {
-                vscode.window.showInformationMessage('Would you like to install Go?', 'Yes', 'No')
-                    .then(selection => {
-                        if (selection === 'Yes') {
-                            terminal.sendText(installGoCommand);
-                        }
-                    });
-            }
-        } else {
-            vscode.window.showInformationMessage(`Go is installed: ${stdout.trim()}`);
-        }
-    });
-}
+// // not needed if someone working with go project will have go installed already on their system
+// function checkGoInstallation(terminal: vscode.Terminal): void {
+//     exec('go version', (error, stdout, stderr) => {
+//         if (error) {
+//             vscode.window.showWarningMessage('Go is not installed. Installing Go is recommended for using Keploy with Go projects.');
+//             const installGoCommand = getGoInstallCommand();
+//             if (installGoCommand) {
+//                 vscode.window.showInformationMessage('Would you like to install Go?', 'Yes', 'No')
+//                     .then(selection => {
+//                         if (selection === 'Yes') {
+//                             terminal.sendText(installGoCommand);
+//                         }
+//                     });
+//             }
+//         } else {
+//             vscode.window.showInformationMessage(`Go is installed: ${stdout.trim()}`);
+//         }
+//     });
+// }
 
-function getGoInstallCommand(): string | null {
-    const platform = os.platform();
-    switch (platform) {
-        case 'darwin':
-            return 'brew install go';
-        case 'linux':
-            return 'sudo apt-get update && sudo apt-get install golang-go';
-        case 'win32':
-            return 'choco install golang';
-        default:
-            vscode.window.showErrorMessage(`Unsupported platform for Go installation: ${platform}`);
-            return null;
-    }
-}
+// function getGoInstallCommand(): string | null {
+//     const platform = os.platform();
+//     switch (platform) {
+//         case 'darwin':
+//             return 'brew install go';
+//         case 'linux':
+//             return 'sudo apt-get update && sudo apt-get install golang-go';
+//         case 'win32':
+//             return 'choco install golang';
+//         default:
+//             vscode.window.showErrorMessage(`Unsupported platform for Go installation: ${platform}`);
+//             return null;
+//     }
+// }
--- src/extension.ts
+++ src/extension.ts
@@ -1,240 +1,263 @@
-import * as vscode from 'vscode';
-import { SidebarProvider } from './SidebarProvider';
-import SignIn, { validateFirst } from './SignIn';
-import oneClickInstall from './OneClickInstall';
-import { getKeployVersion, getCurrentKeployVersion } from './version';
-import { downloadAndUpdate, downloadAndUpdateDocker } from './updateKeploy';
-import Utg from './Utg';
-import { getGitHubAccessToken, getMicrosoftAccessToken, getInstallationID } from './SignIn';
-import * as acorn from 'acorn';
-import * as walk from 'acorn-walk';
+import * as vscode from "vscode";
+import { SidebarProvider } from "./SidebarProvider";
+import SignIn, { validateFirst } from "./SignIn";
+import oneClickInstall from "./OneClickInstall";
+import { getKeployVersion, getCurrentKeployVersion } from "./version";
+import { downloadAndUpdate, downloadAndUpdateDocker } from "./updateKeploy";
+import Utg from "./Utg";
+import {
+	getGitHubAccessToken,
+	getMicrosoftAccessToken,
+	getInstallationID,
+} from "./SignIn";
+import * as acorn from "acorn";
+import * as walk from "acorn-walk";
 
 class KeployCodeLensProvider implements vscode.CodeLensProvider {
-    onDidChangeCodeLenses?: vscode.Event<void> | undefined;
+	onDidChangeCodeLenses?: vscode.Event<void> | undefined;
 
-    provideCodeLenses(
-        document: vscode.TextDocument,
-        token: vscode.CancellationToken
-    ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-        const fileName = document.uri.fsPath;
-        if (fileName.endsWith('.test.js') || fileName.endsWith('.test.ts') || fileName.endsWith('_test.go')) {
-            return [];
-        }
+	provideCodeLenses(
+		document: vscode.TextDocument,
+		token: vscode.CancellationToken
+	): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+		const fileName = document.uri.fsPath;
+		if (
+			fileName.endsWith(".test.js") ||
+			fileName.endsWith(".test.ts") ||
+			fileName.endsWith("_test.go")
+		) {
+			// Added support for Go test files
+			return [];
+		}
 
-        const text = document.getText();
-        const codeLenses: vscode.CodeLens[] = [];
+		const text = document.getText();
+		const codeLenses: vscode.CodeLens[] = [];
 
-        try {
-            if (fileName.endsWith('.go')) {
-                this.parseGoFile(text, document, codeLenses);
-            } else {
-                this.parseJstsFile(text, document, codeLenses);
-            }
-        } catch (error) {
-            console.error(error);
-        }
+		try {
+			const ast = acorn.parse(text, {
+				ecmaVersion: 2020,
+				sourceType: "module",
+			});
 
-        return codeLenses;
-    }
+			walk.fullAncestor(ast, (node: any, state: any, ancestors: any[]) => {
+				if (
+					node.type === "FunctionDeclaration" ||
+					node.type === "FunctionExpression"
+				) {
+					const line = document.positionAt(node.start).line;
+					const range = new vscode.Range(line, 0, line, 0);
+					codeLenses.push(
+						new vscode.CodeLens(range, {
+							title: "🐰 Generate unit tests",
+							command: "keploy.utg",
+							arguments: [document.uri.fsPath],
+						})
+					);
+				} else if (node.type === "ArrowFunctionExpression") {
+					const parent = ancestors[ancestors.length - 2];
+					if (
+						parent.type !== "CallExpression" ||
+						(parent.callee.property?.name !== "then" &&
+							parent.callee.property?.name !== "catch")
+					) {
+						const line = document.positionAt(node.start).line;
+						const range = new vscode.Range(line, 0, line, 0);
+						codeLenses.push(
+							new vscode.CodeLens(range, {
+								title: "🐰 Generate unit tests",
+								command: "keploy.utg",
+								arguments: [document.uri.fsPath],
+							})
+						);
+					}
+				}
+			});
+		} catch (error) {
+			console.error(error);
+		}
 
-    private parseGoFile(text: string, document: vscode.TextDocument, codeLenses: vscode.CodeLens[]) {
-        const functionRegex = /func\s+(\w+)\s*\([^)]*\)\s*(\([^)]*\))?\s*{/g;
-        let match;
-        while ((match = functionRegex.exec(text)) !== null) {
-            const line = document.positionAt(match.index).line;
-            this.addCodeLens(document, line, codeLenses);
-        }
-    }
-
-    private parseJstsFile(text: string, document: vscode.TextDocument, codeLenses: vscode.CodeLens[]) {
-        const ast = acorn.parse(text, { ecmaVersion: 2020, sourceType: 'module' });
-        walk.simple(ast, {
-            FunctionDeclaration: (node: any) => {
-                const line = document.positionAt(node.start).line;
-                this.addCodeLens(document, line, codeLenses);
-            },
-            FunctionExpression: (node: any) => {
-                const line = document.positionAt(node.start).line;
-                this.addCodeLens(document, line, codeLenses);
-            },
-            ArrowFunctionExpression: (node: any) => {
-                const line = document.positionAt(node.start).line;
-                this.addCodeLens(document, line, codeLenses);
-            }
-        });
-    }
-
-    private addCodeLens(document: vscode.TextDocument, line: number, codeLenses: vscode.CodeLens[]) {
-        const range = new vscode.Range(line, 0, line, 0);
-        codeLenses.push(new vscode.CodeLens(range, {
-            title: '🐰 Generate unit tests',
-            command: 'keploy.utg',
-            arguments: [document.uri.fsPath]
-        }));
-    }
+		return codeLenses;
+	}
 }
-
 
 export function activate(context: vscode.ExtensionContext) {
-    const sidebarProvider = new SidebarProvider(context.extensionUri);
-    context.subscriptions.push(
-        vscode.window.registerWebviewViewProvider(
-            "Keploy-Sidebar",
-            sidebarProvider
-        ),
-        vscode.languages.registerCodeLensProvider(
-            [
-                { language: 'javascript', scheme: 'file' },
-                { language: 'typescript', scheme: 'file' },
-                { language: 'go', scheme: 'file' }
-            ],
-            new KeployCodeLensProvider()
-        )
-    );
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"Keploy-Sidebar",
+			sidebarProvider
+		),
+		vscode.languages.registerCodeLensProvider(
+			{ language: "javascript", scheme: "file" },
+			new KeployCodeLensProvider()
+		),
+		vscode.languages.registerCodeLensProvider(
+			{ language: "typescript", scheme: "file" },
+			new KeployCodeLensProvider()
+		),
+		vscode.languages.registerCodeLensProvider(
+			// Added registration for Go language
+			{ language: "go", scheme: "file" },
+			new KeployCodeLensProvider()
+		)
+	);
 
-    oneClickInstall();
+	oneClickInstall();
 
-    let signedIn = context.globalState.get('ourToken');
-    console.log(context.globalState);
-    if (signedIn) {
-        vscode.commands.executeCommand('setContext', 'keploy.signedIn', true);
-        sidebarProvider.postMessage('navigateToHome', 'KeployHome');
-    }
+	let signedIn = context.globalState.get("ourToken");
+	console.log(context.globalState);
+	if (signedIn) {
+		vscode.commands.executeCommand("setContext", "keploy.signedIn", true);
+		sidebarProvider.postMessage("navigateToHome", "KeployHome");
+	}
 
-    // Check if the access token is already present in the global state
-    const accessToken = context.globalState.get<string>('accessToken');
-    // disable if access token is already present
-    if (accessToken) {
-        // Disable the sign-in command since the user is already signed in
-        vscode.commands.executeCommand('setContext', 'keploy.signedIn', true);
-        vscode.window.showInformationMessage('You are already signed in!');
-        // enable the signout command
-        vscode.commands.executeCommand('setContext', 'keploy.signedOut', false);
+	const accessToken = context.globalState.get<string>("accessToken");
+	if (accessToken) {
+		vscode.commands.executeCommand("setContext", "keploy.signedIn", true);
+		vscode.window.showInformationMessage("You are already signed in!");
+		vscode.commands.executeCommand("setContext", "keploy.signedOut", false);
+	} else {
+		vscode.commands.executeCommand("setContext", "keploy.signedOut", true);
+		let signInCommand = vscode.commands.registerCommand(
+			"keploy.SignIn",
+			async () => {
+				try {
+					const result = await getGitHubAccessToken();
 
-    } else {
-        vscode.commands.executeCommand('setContext', 'keploy.signedOut', true);
-        // Register the sign-in command if not signed in
-        let signInCommand = vscode.commands.registerCommand('keploy.SignIn', async () => {
-            try {
-                const result = await getGitHubAccessToken();
+					if (result) {
+						const { accessToken, email } = result;
 
-                if (result) {
-                    const { accessToken, email } = result;
+						getInstallationID();
 
-                    getInstallationID();
+						await context.globalState.update("accessToken", accessToken);
 
-                    // Store the access token in global state
-                    await context.globalState.update('accessToken', accessToken);
+						const { emailID, isValid, error } = await validateFirst(
+							accessToken,
+							"https://api.keploy.io"
+						);
 
-                    const { emailID, isValid, error } = await validateFirst(accessToken, "https://api.keploy.io");
+						vscode.window.showInformationMessage("You are now signed in!");
+						vscode.commands.executeCommand(
+							"setContext",
+							"keploy.signedIn",
+							true
+						);
+						vscode.commands.executeCommand(
+							"setContext",
+							"keploy.signedOut",
+							false
+						);
+					} else {
+						console.log("Failed to get the session or email.");
+						vscode.window.showInformationMessage("Failed to sign in Keploy!");
+					}
+				} catch (error) {
+					vscode.window.showInformationMessage("Failed to sign in Keploy!");
+				}
+			}
+		);
 
-                    // if (isValid) {
-                    vscode.window.showInformationMessage('You are now signed in!');
-                    vscode.commands.executeCommand('setContext', 'keploy.signedIn', true);
-                    vscode.commands.executeCommand('setContext', 'keploy.signedOut', false);
-                    // } else {
-                    //     console.log('Validation failed for the user !');
-                    // }
+		context.subscriptions.push(signInCommand);
+	}
 
-                } else {
-                    console.log('Failed to get the session or email.');
-                    vscode.window.showInformationMessage('Failed to sign in Keploy!');
-                }
-            } catch (error) {
-                // console.error('Error during sign-in:', error);
-                vscode.window.showInformationMessage('Failed to sign in Keploy!');
-            }
-        });
+	let signout = vscode.commands.registerCommand("keploy.SignOut", async () => {
+		context.globalState.update("accessToken", undefined);
+		vscode.window.showInformationMessage("You have been signed out.");
+		vscode.commands.executeCommand("setContext", "keploy.signedIn", false);
+		vscode.commands.executeCommand("setContext", "keploy.signedOut", true);
+	});
 
-        context.subscriptions.push(signInCommand);
-    }
+	context.subscriptions.push(signout);
 
+	let viewKeployVersionDisposable = vscode.commands.registerCommand(
+		"keploy.KeployVersion",
+		async () => {
+			const currentVersion = await getCurrentKeployVersion();
+			vscode.window.showInformationMessage(
+				`The current version of Keploy is ${currentVersion}`
+			);
+		}
+	);
+	context.subscriptions.push(viewKeployVersionDisposable);
 
-    let signout = vscode.commands.registerCommand('keploy.SignOut', async () => {
-        context.globalState.update('accessToken', undefined);
-        vscode.window.showInformationMessage('You have been signed out.');
-        vscode.commands.executeCommand('setContext', 'keploy.signedIn', false);
-        vscode.commands.executeCommand('setContext', 'keploy.signedOut', true);
-    });
+	let viewChangeLogDisposable = vscode.commands.registerCommand(
+		"keploy.viewChangeLog",
+		() => {
+			const changeLogUrl =
+				"https://marketplace.visualstudio.com/items?itemName=Keploy.keployio";
+			vscode.env.openExternal(vscode.Uri.parse(changeLogUrl));
+		}
+	);
+	context.subscriptions.push(viewChangeLogDisposable);
 
-    context.subscriptions.push(signout);
+	let viewDocumentationDisposable = vscode.commands.registerCommand(
+		"keploy.viewDocumentation",
+		() => {
+			const docsUrl = "https://keploy.io/docs/";
+			vscode.env.openExternal(vscode.Uri.parse(docsUrl));
+		}
+	);
+	context.subscriptions.push(viewDocumentationDisposable);
 
-    let viewKeployVersionDisposable = vscode.commands.registerCommand('keploy.KeployVersion', async () => {
-        const currentVersion = await getCurrentKeployVersion();
-        vscode.window.showInformationMessage(`The current version of Keploy is ${currentVersion}`);
-    }
-    );
-    context.subscriptions.push(viewKeployVersionDisposable);
+	let getLatestVersion = vscode.commands.registerCommand(
+		"keploy.getLatestVersion",
+		async () => {
+			const latestVersion = await getKeployVersion();
+			vscode.window.showInformationMessage(
+				`The latest version of Keploy is ${latestVersion}`
+			);
+		}
+	);
+	context.subscriptions.push(getLatestVersion);
 
-    let viewChangeLogDisposable = vscode.commands.registerCommand('keploy.viewChangeLog', () => {
-        const changeLogUrl = 'https://marketplace.visualstudio.com/items?itemName=Keploy.keployio';
-        vscode.env.openExternal(vscode.Uri.parse(changeLogUrl));
-    }
-    );
-    context.subscriptions.push(viewChangeLogDisposable);
+	let updateKeployDisposable = vscode.commands.registerCommand(
+		"keploy.updateKeploy",
+		() => {
+			const options = [
+				{ label: "Keploy Docker", description: "Update using Keploy Docker" },
+				{ label: "Keploy Binary", description: "Update using Keploy Binary" },
+			];
 
-    let viewDocumentationDisposable = vscode.commands.registerCommand('keploy.viewDocumentation', () => {
-        const docsUrl = 'https://keploy.io/docs/';
-        vscode.env.openExternal(vscode.Uri.parse(docsUrl));
-    }
-    );
-    context.subscriptions.push(viewDocumentationDisposable);
+			vscode.window
+				.showQuickPick(options, {
+					placeHolder: "Choose how to update Keploy",
+				})
+				.then(async (selection) => {
+					if (selection) {
+						if (selection.label === "Keploy Docker") {
+							try {
+								await downloadAndUpdateDocker();
+								vscode.window.showInformationMessage("Keploy Docker updated!");
+							} catch (error) {
+								vscode.window.showErrorMessage(
+									`Failed to update Keploy Docker: ${error}`
+								);
+							}
+						} else if (selection.label === "Keploy Binary") {
+							try {
+								await downloadAndUpdate();
+							} catch (error) {
+								vscode.window.showErrorMessage(
+									`Failed to update Keploy binary: ${error}`
+								);
+							}
+						}
+					}
+				});
+		}
+	);
 
+	context.subscriptions.push(updateKeployDisposable);
 
-    let getLatestVersion = vscode.commands.registerCommand('keploy.getLatestVersion', async () => {
-        const latestVersion = await getKeployVersion();
-        vscode.window.showInformationMessage(`The latest version of Keploy is ${latestVersion}`);
-    }
-    );
-    context.subscriptions.push(getLatestVersion);
+	let disposable = vscode.commands.registerCommand(
+		"keploy.utg",
+		async (uri: vscode.Uri) => {
+			vscode.window.showInformationMessage("Welcome to Keploy!");
+			await Utg(context);
+		}
+	);
 
-    let updateKeployDisposable = vscode.commands.registerCommand('keploy.updateKeploy', () => {
-        //open popup to ask user to choose beteween keploy docker or keploy binary
-        const options = [
-            { label: "Keploy Docker", description: "Update using Keploy Docker" },
-            { label: "Keploy Binary", description: "Update using Keploy Binary" }
-        ];
-
-        vscode.window.showQuickPick(options, {
-            placeHolder: "Choose how to update Keploy"
-        }).then(async selection => {
-            if (selection) {
-                // Handle the user's choice here
-                if (selection.label === "Keploy Docker") {
-                    try {
-                        await downloadAndUpdateDocker();
-                        vscode.window.showInformationMessage('Keploy Docker updated!');
-                    } catch (error) {
-                        vscode.window.showErrorMessage(`Failed to update Keploy Docker: ${error}`);
-                    }
-                } else if (selection.label === "Keploy Binary") {
-                    try {
-                        await downloadAndUpdate();
-                        // this._view?.webview.postMessage({ type: 'success', value: 'Keploy binary updated!' });
-                    } catch (error) {
-                        vscode.window.showErrorMessage(`Failed to update Keploy binary: ${error}`);
-                    }
-                }
-            }
-        });
-
-    });
-
-    context.subscriptions.push(updateKeployDisposable);
-
-    // Register the command
-    let disposable = vscode.commands.registerCommand('keploy.utg', async (uri: vscode.Uri) => {
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Welcome to Keploy!');
-
-        await Utg(context);
-    });
-
-    context.subscriptions.push(disposable);
-
+	context.subscriptions.push(disposable);
 }
 
-
-
-export function deactivate() { }
+export function deactivate() {}
